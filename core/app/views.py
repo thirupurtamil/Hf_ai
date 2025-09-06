30638d099765e68b5cace98b24c1be64258eb48a
thirupurtamil <thirupurtamil@gmail.com>
2025-09-06 13:24:14 +0530

    core1

--- core/app/views.py
+++ core/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {
+        "labels": [f"{i*50}/Call Option" if i % 2 == 0 else f"{i*50}/Put Option" for i in range(1, 15)],
+        "values": *14,
+    }
+    return render(request, "index.html", context)
+
