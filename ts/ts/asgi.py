f42187a09e2b2e852ec4ce6d0e737a2f22898ef3
thirupurtamil <thirupurtamil@gmail.com>
2025-09-06 15:56:24 +0530

    27

--- ts/ts/asgi.py
+++ ts/ts/asgi.py
@@ -1,16 +1,16 @@
-"""
-ASGI config for ts project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import home.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ts.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ts.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            home.routing.websocket_urlpatterns
+        )
+    ),
+})
