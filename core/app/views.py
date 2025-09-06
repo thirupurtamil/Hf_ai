0de25b74a428b138a9adbd0eae7109478db7056c
thirupurtamil <thirupurtamil@gmail.com>
2025-09-06 13:32:54 +0530

    core1

--- core/app/views.py
+++ core/app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def index(request):
     context = {
         "labels": [f"{i*50}/Call Option" if i % 2 == 0 else f"{i*50}/Put Option" for i in range(1, 15)],
-        "values":  * 14,  # FIX: 14 zeros; * 14 என்பதற்கு பதில் list repetition
+        "values":  * 15, 
     }
     return render(request, "index.html", context)
 
